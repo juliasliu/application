a41380424551f3551872633fa176c3a2ae640a3f
juliasliu <julia5sliu@gmail.com>
2020-12-05 21:42:18 -0800

    Corrected rows in Cohort Analysis, added percentile calculations

--- flaskr/cohort_analysis.py
+++ flaskr/cohort_analysis.py
@@ -51,6 +51,7 @@ class CohortAnalysis:
         all_months = []
         for year in counter.keys():
             all_months.extend(["{:02d}".format(i+1)+"/"+year for i in range(12)])
+        all_months.pop(0)
         self.missing_months = list(set(all_months) - set(cohort_months))
         last_month = str_to_datetime(cohort_months[-1])
         self.missing_months = list(filter(lambda m: str_to_datetime(m) <= last_month, self.missing_months))
@@ -62,6 +63,10 @@ class CohortAnalysis:
         self.clean_outputs_add_empty_rows(self.cumulative)
 
         col_labels_dict = {col: "M"+str(col+1) for col in self.rev_cohorts.columns}
+
+        self.retention_statistics(self.rev_retention)
+        self.retention_statistics(self.logo_retention)
+        self.retention_statistics(self.cumulative)
 
         indices = [i for i in range(self.cumulative.shape[1]) if "# Customers" != self.cumulative.columns[i]]
 
@@ -83,7 +88,7 @@ class CohortAnalysis:
         self.rev_retention.apply(na_to_zero_list)
         self.rev_retention.apply(zero_to_blank_list)
         self.rev_retention.iloc[:, indices] = self.rev_retention.iloc[:, indices].apply(dec_to_percents_list)
-        self.rev_retention = self.rev_retention.reindex(['# Customers'] + list(self.cumulative.columns[1:-1]), axis=1)
+        self.rev_retention = self.rev_retention.reindex(['# Customers'] + list(self.cumulative.columns[:-1]), axis=1)
         self.rev_retention.reset_index(inplace=True)
         self.rev_retention.rename(columns=col_labels_dict, inplace=True)
 
@@ -91,7 +96,7 @@ class CohortAnalysis:
         self.logo_retention.apply(na_to_zero_list)
         self.logo_retention.apply(zero_to_blank_list)
         self.logo_retention.iloc[:, indices] = self.logo_retention.iloc[:, indices].apply(dec_to_percents_list)
-        self.logo_retention = self.logo_retention.reindex(['# Customers'] + list(self.cumulative.columns[1:-1]), axis=1)
+        self.logo_retention = self.logo_retention.reindex(['# Customers'] + list(self.cumulative.columns[:-1]), axis=1)
         self.logo_retention.reset_index(inplace=True)
         self.logo_retention.rename(columns=col_labels_dict, inplace=True)
 
@@ -99,7 +104,7 @@ class CohortAnalysis:
         self.cumulative.apply(na_to_zero_list)
         self.cumulative.apply(zero_to_blank_list)
         self.cumulative.iloc[:, indices] = self.cumulative.iloc[:, indices].apply(dec_to_dollars_list)
-        self.cumulative = self.cumulative.reindex(['# Customers'] + list(self.cumulative.columns[1:-1]), axis=1)
+        self.cumulative = self.cumulative.reindex(['# Customers'] + list(self.cumulative.columns[:-1]), axis=1)
         self.cumulative.reset_index(inplace=True)
         self.cumulative.rename(columns=col_labels_dict, inplace=True)
 
@@ -115,24 +120,25 @@ class CohortAnalysis:
         print(self.cumulative)
 
     def clean_outputs_add_empty_rows(self, data):
-        print(data)
         for m in self.missing_months:
-            print("change loc")
             data.loc[str_to_datetime(m)] = ['NaN'] * data.shape[1]
         data.sort_index(inplace=True)
         data.index = pd.to_datetime(data.index).strftime('%m/%Y')
 
     def revenue_cohorts(self):
         self.mrr_cohorts = self.mrr.copy()
+        self.mrr_cohorts = self.mrr_cohorts.iloc[:, 1:]
         self.mrr_cohorts.columns = pd.to_datetime(self.mrr_cohorts.columns).strftime('%m/%Y')
         self.mrr_cohorts['Cohort'] = pd.to_datetime(self.cohorts['Cohort'])
         self.rev_cohorts = self.mrr_cohorts.groupby(by=['Cohort']).sum()
+        self.rev_cohorts = self.rev_cohorts.iloc[1:]
         self.rev_cohorts.sort_values('Cohort')
         self.rev_cohorts = self.rev_cohorts.apply(lambda x: pd.Series(x[x != 0].dropna().values), axis=1)
         self.rev_cohorts.set_axis(self.rev_cohorts.columns[self.rev_cohorts.columns], axis=1, inplace=False)
 
     def customer_cohorts(self):
         self.cust_cohorts = self.mrr_cohorts.groupby(by=['Cohort']).agg(lambda x: x.ne(0).sum())
+        self.cust_cohorts = self.cust_cohorts.iloc[1:]
         self.cust_cohorts.sort_values('Cohort')
         self.cust_cohorts = self.cust_cohorts.apply(lambda x: pd.Series(x[x != 0].dropna().values), axis=1)
         self.cust_cohorts.set_axis(self.cust_cohorts.columns[self.cust_cohorts.columns], axis=1, inplace=False)
@@ -141,23 +147,24 @@ class CohortAnalysis:
         self.rev_retention = pd.DataFrame(index=np.arange(self.rev_cohorts.shape[0]))
         self.rev_retention.set_index(self.rev_cohorts.index, inplace=True)
         self.rev_retention = self.rev_cohorts.apply(lambda x: x/x.iloc[0], axis=1)
-        self.rev_retention['# Customers'] = self.cust_cohorts.iloc[:, 0]
-
-        # add min, medium, mean, max percentiles
 
     def logo_retention(self):
         self.logo_retention = pd.DataFrame(index=np.arange(self.cust_cohorts.shape[0]))
         self.logo_retention.set_index(self.cust_cohorts.index, inplace=True)
         self.logo_retention = self.cust_cohorts.apply(lambda x: x/x.iloc[0], axis=1)
-        self.logo_retention['# Customers'] = self.cust_cohorts.iloc[:, 0]
-
-        # add min, medium, mean, max percentiles
 
     def cumulative(self):
         self.cumulative = self.rev_cohorts.copy()
-        self.cumulative.apply(pd.DataFrame.cumsum, axis=1)
+        self.cumulative = self.cumulative.apply(pd.DataFrame.cumsum, axis=1)
         self.cumulative['# Customers'] = self.cust_cohorts.iloc[:, 0]
-        self.cumulative.apply(lambda x: x/x.loc['# Customers'], axis=1)
-        self.cumulative['# Customers'] = self.cust_cohorts.iloc[:, 0]
+        self.cumulative = self.cumulative.apply(lambda x: x/x.loc['# Customers'], axis=1)
 
-        # add min, medium, mean, max percentiles
+    def retention_statistics(self, data):
+        data_copy = data.astype('float64')
+        data.loc["Min"] = data_copy[data_copy != "NaN"].min()
+        data.loc["25th Percentile"] = data_copy.quantile(0.25)
+        data.loc["Median"] = data_copy[data_copy != "NaN"].median()
+        data.loc["Mean"] = data_copy[data_copy != "NaN"].mean()
+        data.loc["75th Percentile"] = data_copy.quantile(0.75)
+        data.loc["Max"] = data_copy[data_copy != "NaN"].max()
+        data['# Customers'] = self.cust_cohorts.iloc[:, 0]
